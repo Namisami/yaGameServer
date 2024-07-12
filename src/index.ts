bbca9db4ef525d5afa4d7c399fb0f9b28ae195a8
Namisami <79804458+Namisami@users.noreply.github.com>
2024-07-12 19:27:01 +0300

    implement socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
-import * as dotenv from "dotenv";
 import * as http from "http";
+import { Server, Socket } from "socket.io";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// Get .env variables
 const PORT = process.env.PORT;
 
-console.log(PORT);
-
+// Realise http server
 const app = http.createServer((req, res) => {
-  res.end("Hello!");
+  switch (req.url) {
+    case "/players":
+      res.end("Players");
+      break;
+    default:
+      res.end("Hello!");
+  }
+});
+
+// Declare WS server
+const socketIO = new Server(app, {
+  cors: {
+    // Origin is only for development
+    origin: "http://localhost:5173"
+  }
+});
+
+const users = [];
+
+socketIO.on("connection", (socket: Socket) => {
+  const user = `User ${users.length}`
+  users.push(user);
+  console.log(`${user} connected`);
+  socketIO.emit("connection", { username: user });
+  socket.on("disconnect", () => {
+    console.log(`${user} disconnected`);
+  });
 });
 
 app.listen(PORT, () => {
